8483cf1c6de0380f50158e674e0b4e8f4dd922f7
sid0486 <sid041103@gmail.com>
2026-04-18 22:40:06 +0530

    feat(error-handling): added try-except and validation logic

--- 7_errorhandling.py
+++ 7_errorhandling.py
@@ -18,15 +18,54 @@ print(divide(10,0))
 
 
 
+number = int(input("Enter number:"))
+
+try :
+    number = int(input("Enter number:"))
+    print(f"You entered: {number}")
+
+except ValueError:
+    print("That's not a number!!")
 
 
 
+try :
+    number = int(input("Enter number :"))
+    result = 100 / number
+    print(result)
+except ValueError:
+    print("That's not a number!")
+except ZeroDivisionError:
+    print("Can't divide by zero")
+except Exception as e :
+    print(f"something unexpected : {e}")
+
+
+try :
+    number = int(input("Enter number :"))
+    result = 100 / number
+except ZeroDivisionError :
+    print("Can't divide by zero")
+except ValueError:
+    print("That's not a number!")
+else :
+    print(f"Result : {result}")
+finally :
+    print("Program finished")
 
 
 
+def check_age(age: int) -> int:
+    if age < 0:
+        raise ValueError("Age cannot be negative!")
+    if age > 150 :
+        raise ValueError("Age is unrealistically high !")
+    return age
 
-
-
+try :
+    check_age(-5)
+except ValueError as e :
+    print(f"Error : {e}")
 
 
 
