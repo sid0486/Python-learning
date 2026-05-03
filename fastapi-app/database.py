63a281fcdb1b018747cb7cd2d98cf77b268b2d3d
sid0486 <sid041103@gmail.com>
2026-05-03 22:54:51 +0530

    add Docker setup - Dockerfile, docker-compose, requirements, database update

--- fastapi-app/database.py
+++ fastapi-app/database.py
@@ -1,7 +1,11 @@
+import os
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-# database.py
-db_url = "postgresql://postgres:sid0402@localhost:5432/fastapi_db"
+db_url = os.getenv(
+    "DATABASE_URL",
+    "postgresql://siddhi:sid0402@localhost:5432/fastapi_db"
+)
+
 engine = create_engine(db_url)
-session = sessionmaker(autocommit = False,autoflush= False,bind = engine)
+session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
