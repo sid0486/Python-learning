c19407d1a42e039f4067c0f980c8f7430201bba8
sid0486 <sid041103@gmail.com>
2026-05-07 21:49:53 +0530

    Add database schema models

--- library-api/src/models.py
+++ library-api/src/models.py
@@ -1,23 +1,18 @@
-from sqlalchemy import Column ,Integer , String , Enum , Date , ForeignKey  
-from database import Base
-# from sqlalchemy.orm import declarative_base
-
-# Base = declarative_base()
+from pydantic import Column , Integer , String , Enum ,ForeignKey , Date
+from sqlalchemy.orm import declarative_base 
+Base = declarative_base()
 
 class Book(Base):
-
     __tablename__ = "book"
 
-    id = Column(Integer,primary_key=True,index=True)
+    id = Column(Integer,primary_key=True,index = True)
     title = Column(String)
     author = Column(String)
     genre = Column(String)
     total_copies = Column(Integer)
     available_copies = Column(Integer)
 
-
 class Member(Base):
-    
     __tablename__ = "member"
 
     id = Column(Integer,primary_key=True,index=True)
@@ -26,37 +21,11 @@ class Member(Base):
     phone = Column(String)
     membership_type = Column(Enum("basic","premium"))
 
-
-
 class Borrow(Base):
-     
     __tablename__ = "borrow"
-
-    id =Column(Integer,primary_key=True,index=True)
+    
+    id = Column(Integer,primary_key=True,index=True)
     book_id = Column(Integer,ForeignKey("book.id"))
     member_id = Column(Integer,ForeignKey("member.id"))
     borrowed_on = Column(Date)
     returned_on = Column(Date,nullable=True)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
